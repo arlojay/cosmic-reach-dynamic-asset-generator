aee65694cb3eeaed469f7d8358d53590905c6762
Arlo Virta <35749102+arlojay@users.noreply.github.com>
2025-03-06 16:00:40 -0600

    Remove dist/

--- dist/api/boilerplate.d.ts
+++
@@ -1,4 +0,0 @@
-import { TriggerSheet } from "./triggerSheet";
-export declare function addDefaultBreakEvents(sheet: TriggerSheet): void;
-export declare function addDefaultPlaceEvents(sheet: TriggerSheet): void;
-export declare function addDefaultEvents(sheet: TriggerSheet): void;
--- dist/api/semantics.d.ts
+++
@@ -1,1 +0,0 @@
-export declare function formatId(id: string, proper?: boolean): string;
