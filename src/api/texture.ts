b93d036b78aaf88934b1b25f4eee0d0fce032af5
arlojay <35749102+arlojay@users.noreply.github.com>
2025-03-15 23:23:41 -0500

    Persuade custom texture instantiation to use util methods

--- src/api/texture.ts
+++ src/api/texture.ts
@@ -1,4 +1,4 @@
-import { createCanvas, Image, loadImage } from "canvas";
+import { Canvas, createCanvas, Image, loadImage } from "canvas";
 import { Mod } from "./mod";
 import { Identifier } from "./identifier";
 
@@ -6,8 +6,14 @@ export class Texture {
     public id: string;
     public texture: Image | string | Identifier = null;
 
-    public static async loadFromFile(id: string, fullPath: string) {
+    public static async loadFromFile(id: string, fullPath: string | Buffer) {
         return new Texture(id, await loadImage(fullPath));
+    }
+    public static async createFromCanvas(id: string, canvas: Canvas) {
+        return await this.loadFromFile(id, canvas.toDataURL());
+    }
+    public static async createFromImage(id: string, image: Image) {
+        return await this.loadFromFile(id, image.src);
     }
 
     constructor(id: string, texture?: Image | string | Identifier) {
@@ -19,18 +25,16 @@ export class Texture {
         return "textures/blocks/" + this.id + ".png";
     }
     public getAsBlockTextureId(mod: Mod): Identifier {
-        if(this.texture != null && !(this.texture instanceof Image)) {
-            return this.texture instanceof Identifier ? this.texture : Identifier.fromId(this.texture);
-        }
+        if(this.texture instanceof Identifier) return this.texture.derive("textures/blocks/" + this.texture.getItem() + ".png");
+        if(typeof this.texture == "string") return Identifier.fromId(this.texture);
         return new Identifier(mod, "textures/blocks/" + this.id + ".png");
     }
     public getAsItemTexturePath(): string {
         return "textures/items/" + this.id + ".png";
     }
     public getAsItemTextureId(mod: Mod): Identifier {
-        if(this.texture != null && !(this.texture instanceof Image)) {
-            return this.texture instanceof Identifier ? this.texture : Identifier.fromId(this.texture);
-        }
+        if(this.texture instanceof Identifier) return this.texture.derive("textures/items/" + this.texture.getItem() + ".png");
+        if(typeof this.texture == "string") return Identifier.fromId(this.texture);
         return new Identifier(mod, "textures/items/" + this.id + ".png");
     }
     public createTextureStream() {
